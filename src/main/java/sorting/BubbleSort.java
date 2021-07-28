322106445bd76004aa09fa18288e0953f806a4e5
Krissada Jindanupajit <jindanupajit@gmail.com>
2021-07-28 09:53:04 -0400

    Bubble Sort

---
+++ src/main/java/sorting/BubbleSort.java
@@ -0,0 +1,18 @@
+package sorting;
+
+public class BubbleSort  {
+    
+    public int[] sort(int[] data) {
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                if (data[i] < data[j]) {
+                    int t = data[i];
+                    data[i] = data[j];
+                    data[j] = t;
+                }
+            }
+        }
+        return data;
+    }
+}
